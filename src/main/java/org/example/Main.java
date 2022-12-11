983a75976c586e49b27fd054c4b223d8a3db34a1
fiqri khoirul m <fiqrikm18@gmail.com>
2022-12-11 21:35:30 +0700

    simple apache kafka producer with java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args ) {
+        try {
+            Properties kafkaProps = new Properties();
+            kafkaProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+            kafkaProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            kafkaProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+            KafkaProducer<String, String> producer = new KafkaProducer<String, String>(kafkaProps);
+            ProducerRecord<String, String> record = new ProducerRecord<>("user_ids", "Biomedical Materials", "USA");
+            producer.send(record).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/ProducerCallback.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class ProducerCallback implements Callback {
+    @Override
+    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+        if (e != null) {
+            e.printStackTrace();
+        }
+    }
+}
